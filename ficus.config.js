ab9cf5fb658ac7b3dd055240dd42f71fd26fbd50
omridevk <omrikatz12@gmail.com>
2022-08-09 22:50:20 +0300

    third commit

--- ficus.config.js
+++ ficus.config.js
@@ -3,7 +3,6 @@ export default {
     output: "test",
     figma: {
         pageName: "icon",
-        token: "figd_5zIKPDuv9evo99advUGxapg21rJdf_Hm0AAiMVTt",
         fileKey: "V4eZU3qDgKYPhR4eaTvSwy",
         imageKey: "V4eZU3qDgKYPhR4eaTvSwy",
     },
